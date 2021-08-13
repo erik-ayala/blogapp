a7726aa97a9448ccf57f9144ee535be24c77dae5
Erik Ayala <ayala.erik61@gmail.com>
2021-08-13 09:16:53 -0500

    exercises

---
+++ src/main/java/com/codeup/blogapp/web/PostController.java
@@ -0,0 +1,48 @@
+package com.codeup.blogapp.web;
+
+import com.codeup.blogapp.data.Post;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/posts", headers = "Accept=application/json")
+public class PostController {
+
+    @GetMapping
+    private List<Post> getPosts() {
+        return new ArrayList<>() {{
+            add(new Post(1L, "A New Post.",
+                    "this is a brilliant post."));
+        }};
+    }
+
+    @GetMapping("{id}")
+    private Post getPostById(@PathVariable Long id) {
+        if (id == 1) {
+            return new Post(1L, "A new Post",
+                    "this is a brilliant post");
+        } else {
+            return null;
+        }
+    }
+
+    @PostMapping("/posts")
+    private void createPost(@RequestBody Post post) {
+        System.out.println(post);
+    }
+
+    @PutMapping("/{id}")
+    private void updatePost(@PathVariable Long id, @RequestBody Post post) {
+        System.out.println(id);
+        System.out.println(post);
+    }
+
+    @DeleteMapping("/{id}")
+    private void deletePost(@PathVariable Long id) {
+        System.out.println(id);
+
+    }
+}
+
