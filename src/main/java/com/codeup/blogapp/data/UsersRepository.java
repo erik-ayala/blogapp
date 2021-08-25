5b4894f14a8b3d592858e45347f754fa3da41ce3
Erik Ayala <ayala.erik61@gmail.com>
2021-08-25 10:56:06 -0500

    errors

--- src/main/java/com/codeup/blogapp/data/UsersRepository.java
+++ src/main/java/com/codeup/blogapp/data/UsersRepository.java
@@ -2,6 +2,9 @@ package com.codeup.blogapp.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 
 }
